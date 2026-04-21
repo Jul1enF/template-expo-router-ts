f1dbe9030be67750c532032e0de8e4476e6a230c
Jul1enF <j.furic@gmail.com>
2026-04-21 16:34:28 +0200

    removed one of the autocomplete example

--- app/(tabs)/tab2/index.tsx
+++ app/(tabs)/tab2/index.tsx
@@ -8,12 +8,6 @@ import { appStyle } from '@styles/appStyle';
 
 export default function Tab2Page() {
 
-  const employeeList = [{"employee": {"__v": 9, "_id": "694046fa9c31680cd45cf9b1", "contract_end": null, "createdAt": "2025-12-15T17:35:54.181Z", "email": "ju@ju.ju", "first_name": "Julien", "last_name": "Furic", "role": "owner", "updatedAt": "2026-04-20T08:33:13.943Z"}, "title": "Julien Furic"}, {"employee": {"__v": 7, "_id": "69404c019c31680cd45cf9b6", "contract_end": null, "createdAt": "2025-12-15T17:57:21.449Z", "email": "fu@fu.fu", "first_name": "Furic", "last_name": "De Concarneau", "role": "owner", "updatedAt": "2026-02-12T05:52:23.372Z"}, "title": "Furic De Concarneau"}, {"employee": {"__v": 0, "_id": "69404c6a9c31680cd45cf9b9", "contract_end": null, "createdAt": "2025-12-15T17:59:06.923Z", "email": "sy@sy.sy", "first_name": "Chloé", "last_name": "Chataigne", "role": "owner", "updatedAt": "2026-02-13T13:23:52.690Z"}, "title": "Chloé Chataigne"}, {"employee": {"__v": 0, "_id": "6940503c9c31680cd45cf9ce", "contract_end": null, "createdAt": "2025-12-15T18:15:24.060Z", "email": "fa@fa.fa", "first_name": "Fanny", "last_name": "Farance", "role": "employee", "updatedAt": "2025-12-21T22:41:34.502Z"}, "title": "Fanny Farance"}]
-
-  const [selectedEmployee, setSelectedEmployee]=useState<null | unknown>(null)
-  // console.log("selected employee :", selectedEmployee)
-
-
   const employeeList2 = [{"__v": 9, "_id": "694046fa9c31680cd45cf9b1", "contract_end": null, "createdAt": "2025-12-15T17:35:54.181Z", "email": "ju@ju.ju", "first_name": "Julien", "last_name": "Furic", "role": "owner", "updatedAt": "2026-04-20T08:33:13.943Z"}, {"__v": 7, "_id": "69404c019c31680cd45cf9b6", "contract_end": null, "createdAt": "2025-12-15T17:57:21.449Z", "email": "fu@fu.fu", "first_name": "Furic", "last_name": "De Concarneau", "role": "owner", "updatedAt": "2026-02-12T05:52:23.372Z"}, {"__v": 0, "_id": "69404c6a9c31680cd45cf9b9", "contract_end": null, "createdAt": "2025-12-15T17:59:06.923Z", "email": "sy@sy.sy", "first_name": "Chloé", "last_name": "Chataigne", "role": "owner", "updatedAt": "2026-02-13T13:23:52.690Z"}, {"__v": 0, "_id": "6940503c9c31680cd45cf9ce", "contract_end": null, "createdAt": "2025-12-15T18:15:24.060Z", "email": "fa@fa.fa", "first_name": "Fanny", "last_name": "Farance", "role": "employee", "updatedAt": "2025-12-21T22:41:34.502Z"}]
 
   const [selectedEmployee2, setSelectedEmployee2]=useState<null | unknown>(null)
@@ -33,16 +27,6 @@ export default function Tab2Page() {
         <View style={{ minHeight: 2000, width : "100%", alignItems : "center" }}>
 
           <Text style={styles.pageTitle}>Tab 2 !</Text>
-
-          <Autocomplete
-          data={employeeList}
-          setSelectedItem={setSelectedEmployee}
-          selectedItem={selectedEmployee}
-          sectionToSelectKey='employee'
-          inputStyle={[appStyle.input.baseLarge]}
-          placeholderText='Rechercher un employé'
-          placeholderColor="black"
-          />
 
           <Autocomplete
           data={employeeList2}
